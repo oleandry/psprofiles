cdbf39c21fdc8850f29c610cef8a757266fd5aa0
Ole Andreas Rydland <ole.andreas.rydland@if.no>
2017-01-17 14:16:54 +0100

    added location functions

---
+++ wp-inv.psm1
@@ -0,0 +1,35 @@
+function wp {
+  Set-Location C:\waypointgit
+}
+
+function als {
+  Set-Location C:\waypointgit\AccountsLedger
+}
+
+function pi {
+  Set-Location C:\waypointgit\PolicyInvoicing
+}
+
+function ipm {
+  Set-Location C:\waypointgit\InvoicePresentationModel
+}
+
+function ir {
+  Set-Location C:\waypointgit\InvoiceReminder
+}
+
+function inv.wc {
+  Set-Location C:\waypointgit\Invoicing.WebComponents
+}
+
+function inv.util {
+  Set-Location C:\waypointgit\Invoicing.Utilities
+}
+
+function inv.int {
+  Set-Location C:\waypointgit\Invoicing.Internal
+}
+
+function inv.view {
+  Set-Location C:\waypointgit\Invoicing.View
+}
